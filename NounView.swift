3e48bc523444dd2bbb78e357a43c8315f5d2cd9f
carina gatta <cgatta6130@stu.d214.org>
2023-04-06 14:11:56 -0500

    added NounView and created navigationLink yo Verbview

---
+++ NounView.swift
@@ -0,0 +1,17 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by Carina Gatta on 4/6/23.
+//
+
+
+import SwiftUI
+struct NounView: View {
+    var body: some View {
+        NavigationLink("VerbView") {
+            VerbView()
+        }
+    }
+}
+
