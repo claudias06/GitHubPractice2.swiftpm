821ed9ef28802217849ff74ffce8b9c636feed60
Claudia <csepko4429@stu.d214.org>
2023-04-06 14:24:37 -0500

    made SummaryView with s sentence using a noun, verb, and adjective

---
+++ SummaryView.swift
@@ -0,0 +1,14 @@
+//
+//  SummaryView.swift
+//  GitHubPractice2
+//
+//  Created by Claudia Sepko on 4/6/23.
+//
+
+import SwiftUI
+struct SummaryView: View {
+    var body: some View {
+        Text("The \(noun) decided to \(verb) in the \(adjective) car")
+    }
+}
+
