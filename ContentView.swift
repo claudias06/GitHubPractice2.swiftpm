ef20499b5d51842d9c322b54a999bc6b268b0d15
Claudia <csepko4429@stu.d214.org>
2023-04-06 14:11:43 -0500

    made navigation link and navigation view

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            NavigationLink("NounView"){
+                NounView()
+            }
         }
     }
 }
