b9e326fccd1c12bbe73825e35bbdf519f8d79bfc
Claudia <csepko4429@stu.d214.org>
2023-04-06 14:18:32 -0500

    created VerbView with navigation link to Adjective View

---
+++ VerbView.swift
@@ -0,0 +1,16 @@
+//
+//  VerbView.swift
+//  GitHubPractice2
+//
+//  Created by Claudia Sepko on 4/6/23.
+//
+
+import SwiftUI
+struct VerbView: View {
+    var body: some View {
+        NavigationLink("AdjectiveView") {
+            AdjectiveView()
+        }
+    }
+}
+
