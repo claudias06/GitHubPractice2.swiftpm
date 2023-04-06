46b3cfbffcf0d8ac54c466881b716f0391ef5286
carina gatta <cgatta6130@stu.d214.org>
2023-04-06 14:30:29 -0500

    did MadLib view

---
+++ MadLib.swift
@@ -0,0 +1,18 @@
+//
+//  MadLib.swift
+//  GitHubPractice2
+//
+//  Created by Carina Gatta on 4/6/23.
+//
+
+import SwiftUI
+struct MadLib {
+   
+    var noun = ""
+   var verb = ""
+   var adjective = ""
+    
+}
+
+   
+
